3400b6f301b62d64b855ee54f21e3b351bc3aa1a
braddavistech <brad@braddavistech.com>
2018-10-03 14:49:56 -0500

    updates for js to run input form

--- inputFormTest.js
+++ inputFormTest.js
@@ -3,11 +3,9 @@ var allUsersId = ["firstName", "lastName", "userEmail", "userEmail2", "streetAdd
 var allUsers = {firstName : "First", lastName : "Last", userEmail : "Email", userEmail2 : "Email 2", streetAddress : "Street Address", city : "City", state : "State", zip : "Zip", dob : "Date of Birth", anniversary : "Anniversary", favStore : "Favorite Store", favRestaurant : "Favorite Restaurant", favFood : "Favorite Food", hobbies : "Hobbies"};
 
 function onStart() {
-  console.log(allUsersId.length);
   var newItems = document.createElement("p");
-  var node = document.createTextNode("This is a test");
+  var node = document.createTextNode("WE ARE GLAD YOU ARE JOINING");
   newItems.appendChild(node);
-  console.log(newItems);
   document.getElementById("title").appendChild(newItems);
   document.getElementById("footer").innerHTML = "Brad Davis, 2018";
 };
@@ -23,46 +21,74 @@ function createInputForm (){
     newInput.type = "text";
     newInput.className = "inputTags";
     newInput.id = allUsersId[i];
-    inputNameChange(newInput.id);
     newInput.size = "50";
-    newInput.placeholder = allUsersId[i];
+    if (allUsersId[i] == "firstName"){
+      newInput.placeholder = "Enter Your First Name.";
+    } else if (allUsersId[i] == "lastName"){
+      newInput.placeholder = "Enter Your Last Name.";
+    }else if (allUsersId[i] == "userEmail"){
+      newInput.placeholder = "Enter Your Email Address.";
+    }else if (allUsersId[i] == "userEmail2"){
+      newInput.placeholder = "Enter Your Secondary Email Address.";
+    }else if (allUsersId[i] == "streetAddress"){
+      newInput.placeholder = "Enter Your Street Address.";
+    }else if (allUsersId[i] == "city"){
+      newInput.placeholder = "Enter Your City.";
+    }else if (allUsersId[i] == "state"){
+      newInput.placeholder = "Enter Your State.";
+    }else if (allUsersId[i] == "zip"){
+      newInput.placeholder = "Enter Your Zip Code.";
+    }else if (allUsersId[i] == "dob"){
+      newInput.placeholder = "Enter Your Date Of Birth.";
+    }else if (allUsersId[i] == "anniversary"){
+      newInput.placeholder = "Enter Your Anniversary Date.";
+    }else if (allUsersId[i] == "favStore"){
+      newInput.placeholder = "Enter Your Favorite Store.";
+    }else if (allUsersId[i] == "favRestaurant"){
+      newInput.placeholder = "Enter Your Favorite Restaurant.";
+    }else if (allUsersId[i] == "favFood"){
+      newInput.placeholder = "Enter Your Favorite Food.";
+    }else if (allUsersId[i] == "hobbies"){
+      newInput.placeholder = "Enter Your Hobbies and Interests.";
+    }
     newForm.appendChild(newInput);
   };
+
   document.getElementById("testInput").appendChild(newForm);
-  console.log(newForm);
 };
 
-function inputNameChange(testName){
-  if (testName = "firstName"){
-    console.log("Enter Your First Name.")
-  } else if (testName = "lastName"){
-    console.log("Enter Your Last Name.")
-  } else if (testName = "userEmail"){
-    console.log("Enter Your Email Address.")
-  } else if (testName = "userEmail2"){
-    console.log("Enter Your Secondary Email Address.")
-  } else if (testName = "streetAddress"){
-    console.log("Enter Your Street Address.")
-  } else if (testName = "city"){
-    console.log("Enter Your City.")
-  } else if (testName = "state"){
-    console.log("Enter Your State.")
-  } else if (testName = "zip"){
-    console.log("Enter Your Zip.")
-  } else if (testName = "dob"){
-    console.log("Enter Your Date Of Birth.")
-  } else if (testName = "anniversary"){
-    console.log("Enter Your Anniversay Date.")
-  } else if (testName = "favStore"){
-    console.log("Enter Your Favorite Store.")
-  } else if (testName = "favRestaurant"){
-    console.log("Enter Your Favorite Restaurant.")
-  } else if (testName = "favFood"){
-    console.log("Enter Your Favorite Food")
-  } else if (testName = "hobbies"){
-    console.log("Enter Your Hobbies.")
-  };
-};
+// function inputNameChange(testName){
+//   if (testName = "firstName"){
+//     console.log("Enter Your First Name.");
+//     return newInput.placeholder
+//   } else if (testName = "lastName"){
+//     console.log("Enter Your Last Name.")
+//   } else if (testName = "userEmail"){
+//     console.log("Enter Your Email Address.")
+//   } else if (testName = "userEmail2"){
+//     console.log("Enter Your Secondary Email Address.")
+//   } else if (testName = "streetAddress"){
+//     console.log("Enter Your Street Address.")
+//   } else if (testName = "city"){
+//     console.log("Enter Your City.")
+//   } else if (testName = "state"){
+//     console.log("Enter Your State.")
+//   } else if (testName = "zip"){
+//     console.log("Enter Your Zip.")
+//   } else if (testName = "dob"){
+//     console.log("Enter Your Date Of Birth.")
+//   } else if (testName = "anniversary"){
+//     console.log("Enter Your Anniversay Date.")
+//   } else if (testName = "favStore"){
+//     console.log("Enter Your Favorite Store.")
+//   } else if (testName = "favRestaurant"){
+//     console.log("Enter Your Favorite Restaurant.")
+//   } else if (testName = "favFood"){
+//     console.log("Enter Your Favorite Food")
+//   } else if (testName = "hobbies"){
+//     console.log("Enter Your Hobbies.")
+//   };
+// };
 
 function createUser(){
     var fName = document.getElementById("firstName").value;
